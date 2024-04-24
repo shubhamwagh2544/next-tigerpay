4b7fcb2a9d624db1b6425c8e36f12ef7a263ee46
shubham.wagh <shubham.wagh@globant.com>
2024-04-24 16:33:53 +0530

    prisma config added

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,16 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { PrismaClient } from "@repo/db/client";
+const prisma = new PrismaClient();
 
 export default function Page(): JSX.Element {
+  async function test() {
+    await prisma.user.findUnique({
+      where: {
+        id: 1
+      }
+    })
+  }
   return (
-    <div className="text-2xl font-bold">
+    <div className="text-2xl">
       Hello World
     </div>
   );
